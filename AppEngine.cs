2f14dfe7df8f3cba36fa347c58f8f0ae5b48b199
Artem-PC <xtergs@gmail.com>
2014-08-24 23:27:54 +0300

    Desining

--- AppEngine.cs
+++ AppEngine.cs
@@ -5,16 +5,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Documents;
+using VideoFileRenamer.Annotations;
 
 namespace VideoFileRenamer
 {
 	class AppEngine
 	{
-		List<FileInfo> FindVideo(string path)
+
+		//Singleton
+		private static AppEngine current;
+
+		private List<string> ignoringFiles = new List<string>();
+		List<string> storedFiles = new List<string>(); //BD
+
+		public AppEngine Create()
 		{
-			List<FileInfo> list = new List<FileInfo>();
-			var fils = Directory.EnumerateFiles(path);
+			if (current == null)
+				current = new AppEngine();
+			return current;
+		}
+
+		//Возвращает список новых фильмов и серий для сериалов
+		public List<FileVideoInfo> FindNewVideos(string path)
+		{
+			List<FileVideoInfo> list = new List<FileVideoInfo>();
+			foreach (var file in Directory.EnumerateFiles(path))
+			{
+				FileInfo infoFile = new FileInfo(file);
+				if (!ignoringFiles.Contains(infoFile.Name) || !storedFiles.Contains(infoFile.Name))
+					list.Add(new FileVideoInfo(infoFile));
+			}
 			return list;
 		}
+
+		//Подсчитывает хэши файлов
+		public void CalculateHashFiles(List<FileVideoInfo> list)
+		{
+			if (list == null) 
+				throw new ArgumentNullException("list in AppEngine.CalculateHashFiles");
+
+			foreach (var fileVideoInfo in list)
+			{
+				fileVideoInfo.CalculateHash();
+			}
+		}
+
+		//Поиск фильма на кинопоиске
+		public  FindFilms()
+		{
+			
+		}
+
+		//
+		public void DownloadInfoFilmsAsync(List<FileVideoInfo> list)
+		{ }
 	}
 }
